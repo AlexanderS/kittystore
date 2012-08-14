7c3e8965058df897f969a1b67402804f28e60373
Alexander Sulfrian <alexander@sulfrian.net>
2012-08-14 21:09:56 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='KittyStore',
+      version='0.1',
+      description='Interface for different storage solution for mailman3 and API to access the information.',
+      author='Pierre-Yves Chibon',
+      author_email='pingou@pingoured.fr',
+      url='https://github.com/syst3mw0rm/kittystore',
+      packages=['kittystore'],
+      )
